7e21d980d5d57931f3e0d867f1bb4d3ff8172d10
Ricardo Pandales <rpandales@pros.com>
2016-09-26 18:05:28 -0500

    Add basic offline support

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,8 +20,8 @@ angular
 
       if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js')
-          .then(function (registration) {
-            console.log(registration);
+          .then(function () {
+            console.log('service worker registered');
           })
           .catch(function (m) {
             console.log(m)
